64816cfdc0bf2da2b6e612d2d116a35bb6639fff
Arne Schmid <arne.schmid@arm.com>
2026-03-13 14:02:49 +0100

    Remove redundant 'activated' property from solution load state factories

--- src/solutions/solution-manager.factories.ts
+++ src/solutions/solution-manager.factories.ts
@@ -29,7 +29,6 @@ export const idleSolutionLoadStateFactory = makeFactory<SolutionLoadState>({
     activated: () => false,
     loaded: () => undefined,
     converted: () => undefined,
-    activated: () => undefined,
 });
 
 export const activeSolutionLoadStateFactory = makeFactory<SolutionLoadState>({
@@ -37,7 +36,6 @@ export const activeSolutionLoadStateFactory = makeFactory<SolutionLoadState>({
     activated: () => true,
     loaded: () => undefined,
     converted: () => undefined,
-    activated: () => true,
 });
 
 const fireOnDidChangeLoadState = (emitter: vscode.EventEmitter<SolutionLoadStateChangeEvent>) => {
